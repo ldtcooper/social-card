43276323ae7790d3722717871f8854b99bbf2343
Logan Cooper <ldtcoop@gmail.com>
2017-11-28 15:02:11 -0800

    Finish content component

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Content = (props) => {
-
+  return(
+    <div className='content-box'>
+      <p className='text-content'>{props.textContent}</p>
+      <img className='image-content' src={props.imageContent}/>
+    </div>
+  );
 };
 
 export default Content;

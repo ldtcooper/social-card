94f445e737bfc82b471e8643e9e16da4ad88aa5b
Logan Cooper <ldtcoop@gmail.com>
2017-11-29 11:46:19 -0800

    Make date component real-time

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,6 +4,52 @@ import UserInfo from './user_info.jsx';
 class Header extends Component {
   constructor(props) {
     super(props);
+    this.state = {date: null};
+  }
+
+  componentWillMount() {
+    const d = new Date();
+    let month;
+    switch (d.getMonth()) {
+      case 0:
+        month = "Jan";
+        break;
+      case 1:
+        month = "Feb";
+        break;
+      case 2:
+        month = "Mar";
+        break;
+      case 3:
+        month = "Apr";
+        break;
+      case 4:
+        month = "May";
+        break;
+      case 5:
+        month = "Jun";
+        break;
+      case 6:
+        month = "Jul";
+        break;
+      case 7:
+        month = "Aug";
+        break;
+      case 8:
+        month = "Sep";
+        break;
+      case 9:
+        month = "Oct";
+        break;
+      case 10:
+        month = "Nov";
+        break;
+      case 11:
+        month = "Dec";
+        break;
+    }
+    const dd = d.getDate();
+    this.setState({date: `${month} ${dd}`});
   }
 
   render() {
@@ -12,7 +58,7 @@ class Header extends Component {
       <header>
         <img src='https://picsum.photos/100/100/?random' alt={`${this.props.username}'s avatar'`}/>
         <div className='header-content-wrapper'>
-          <UserInfo username={this.props.username} userHandle={this.props.userHandle} date={this.props.date}/>
+          <UserInfo username={this.props.username} userHandle={this.props.userHandle} date={this.state.date}/>
           <p className='text-content'>{this.props.textContent}</p>
         </div>
       </header>

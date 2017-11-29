ddf1115727f66a26075b2f6f4d17ca4c897edd02
Logan Cooper <ldtcoop@gmail.com>
2017-11-28 16:33:57 -0800

    Add ids for buttons

--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -4,9 +4,9 @@ import { SocialButton, MailButton} from './social_button.jsx';
 const Toolbar = (props) => {
     return(
       <footer>
-        <SocialButton icon="fa fa-comment-o"/>
-        <SocialButton icon="fa fa-retweet"/>
-        <SocialButton icon="fa fa-heart-o"/>
+        <SocialButton id="comment" icon="fa fa-comment-o"/>
+        <SocialButton id="retweet" icon="fa fa-retweet"/>
+        <SocialButton id="heart" icon="fa fa-heart-o"/>
         <MailButton />
       </footer>
     );

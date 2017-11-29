6b3cee5b1ed365a01833e315d49a5eeb93756643
Logan Cooper <ldtcoop@gmail.com>
2017-11-28 20:22:36 -0800

    Add interactivity to buttons

--- src/components/social_button.jsx
+++ src/components/social_button.jsx
@@ -4,11 +4,24 @@ export class SocialButton extends Component {
   constructor(props) {
     super(props);
     this.state={clicks: Math.floor(50 * Math.random())};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const elemId = e.target.parentElement.getAttribute("id");
+    if (e.target.parentElement.getAttribute("id").match(/-clicked/)) {
+      const unclicked = elemId.split("-")[0];
+      this.setState({clicks: this.state.clicks - 1});
+      e.target.parentElement.setAttribute("id", `${unclicked}`);
+    } else {
+      this.setState({clicks: this.state.clicks + 1});
+      e.target.parentElement.setAttribute("id", `${elemId}-clicked`);
+    }
   }
 
   render() {
     return(
-      <div className="social-button" id={this.props.id}>
+      <div className="social-button" id={this.props.id} onClick={this.handleClick}>
         <i className={[`fa ${this.props.icon}`, 'social-icon'].join(" ")} aria-hidden="true"></i>
         <p className='interaction-count'>{this.state.clicks}</p>
       </div>
@@ -16,10 +29,26 @@ export class SocialButton extends Component {
   }
 }
 
-export const MailButton = (props) => {
-  return(
-    <div className="social-button" id={props.id}>
-      <i className="fa fa-envelope-o social-icon" aria-hidden="true"></i>
-    </div>
-  );
-};
+export class MailButton extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const elemId = e.target.parentElement.getAttribute("id");
+    if (elemId === "mail") {
+      e.target.parentElement.setAttribute("id", "mail-clicked");
+    } else {
+      e.target.parentElement.setAttribute("id", "mail");
+    }
+  }
+
+  render() {
+    return(
+      <div className="social-button" id={this.props.id} onClick={this.handleClick}>
+        <i className="fa fa-envelope-o social-icon" aria-hidden="true"></i>
+      </div>
+    );
+  }
+}

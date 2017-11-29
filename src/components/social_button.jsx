e5e739365903123b06a1854360bebc5a08b57050
Logan Cooper <ldtcoop@gmail.com>
2017-11-28 18:52:06 -0800

    Add random numbers to social buttons

--- src/components/social_button.jsx
+++ src/components/social_button.jsx
@@ -3,14 +3,14 @@ import React, {Component} from 'react';
 export class SocialButton extends Component {
   constructor(props) {
     super(props);
-    this.state={};
+    this.state={clicks: Math.floor(50 * Math.random())};
   }
 
   render() {
     return(
       <div className="social-button" id={this.props.id}>
         <i className={[`fa ${this.props.icon}`, 'social-icon'].join(" ")} aria-hidden="true"></i>
-        <p className='interaction-count'>3</p>
+        <p className='interaction-count'>{this.state.clicks}</p>
       </div>
     );
   }

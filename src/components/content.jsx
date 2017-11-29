9f569c3aab555b6919702363e2472d429e349019
Logan Cooper <ldtcoop@gmail.com>
2017-11-28 16:12:23 -0800

    Align all items

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -4,7 +4,7 @@ const Content = (props) => {
   return(
     <div className='content-box'>
       <p className='text-content'>{props.textContent}</p>
-      <img className='image-content' src={props.imageContent} alt="Content"/>
+      {props.imageContent ? <img className='image-content' src={props.imageContent} alt="Content"/> : ""}
     </div>
   );
 };

f041524a56852b8fd32f8be475391bb2b2d95f11
Logan Cooper <ldtcoop@gmail.com>
2017-11-29 11:24:10 -0800

    Refactor avatar generation into avatar component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,7 +10,7 @@ class Header extends Component {
     return(
       // takes avatarLink, username, userHandle, and date
       <header>
-        <img src={this.props.avatarLink} alt={`${this.props.username}'s avatar'`}/>
+        <img src='https://picsum.photos/100/100/?random' alt={`${this.props.username}'s avatar'`}/>
         <div className='header-content-wrapper'>
           <UserInfo username={this.props.username} userHandle={this.props.userHandle} date={this.props.date}/>
           <p className='text-content'>{this.props.textContent}</p>
